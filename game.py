779f210e9fa052624584794d6bfbd6e644eb8c4e
Valentino Ongaro <valentinoongaro@hotmail.com>
2024-03-21 19:30:39 -0300

    El juego pasa a tener fallos en vez de intentos

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ words = ["python", "programación", "computadora", "código", "desarrollo",
 # Elegir una palabra al azar
 secret_word = random.choice(words)
 # Número máximo de intentos permitidos
-max_attempts = 10
+max_mistakes = 5
 # Lista para almacenar las letras adivinadas
 guessed_letters = []
 print("¡Bienvenido al juego de adivinanzas!")
@@ -13,7 +13,7 @@ print("Estoy pensando en una palabra. ¿Puedes adivinar cuál es?")
 word_displayed = "_" * len(secret_word)
 # Mostrarla palabra parcialmente adivinada
 print(f"Palabra: {word_displayed}")
-for i in range(max_attempts):
+while max_mistakes>0:
  # Pedir al jugador que ingrese una letra
  letter = input("Ingresa una letra: ").lower()
  # Verificar si la letra ya ha sido adivinada
@@ -29,6 +29,7 @@ for i in range(max_attempts):
     print("¡Bien hecho! La letra está en la palabra.")
  else:
     print("Lo siento, la letra no está en la palabra.")
+    max_mistakes = max_mistakes - 1
  # Mostrar la palabra parcialmente adivinada
  letters = []
  for letter in secret_word:
@@ -43,5 +44,5 @@ for i in range(max_attempts):
     print(f"¡Felicidades! Has adivinado la palabra secreta: {secret_word}")
     break
 else:
-    print(f"¡Oh no! Has agotado tus {max_attempts} intentos.")
+    print(f"¡Oh no! Has agotado tus fallos.")
     print(f"La palabra secreta era: {secret_word}")
